c32b30dce15616f9f5cffcd87f515ada35458387
Pedro Moreira <phribasmoreira@gmail.com>
2023-01-07 12:14:27 +0000

    added modal to add new book details

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// $("#exampleModalCenter").on("shown.bs.modal", function () {
+//   $("#myInput").trigger("focus");
+// });
+
 checkBtn = document.querySelector(".checkBtn");
 checkBtn.addEventListener("click", () => {
   checkBtn.classList.toggle("active");
@@ -6,4 +10,23 @@ checkBtn.addEventListener("click", () => {
   } else {
     checkBtn.innerText = "Mark as read";
   }
+});
+
+const inputAuthor = document.querySelector("#inputAuthor");
+const inputDate = document.querySelector("#inputDate");
+const inputTitle = document.querySelector("#inputTitle");
+const inputImg = document.querySelector("#inputImg");
+const inputRating = document.querySelector("#inputRating");
+const isRead = document.querySelector("#isRead");
+
+document.querySelector("#addBookBtn").addEventListener("click", () => {
+  console.log(isRead.value);
+
+  console.log(`inputAuthor: ${inputAuthor.value}; 
+  inputImg: ${inputImg.value}; 
+  inputTitle: ${inputTitle.value}; 
+  inputDate: ${inputDate.value}; 
+  inputRating: ${inputRating.value}; 
+  isRead: ${isRead.value};
+  `);
 });
